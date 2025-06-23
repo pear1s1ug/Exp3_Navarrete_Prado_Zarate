5cf137aca48e3ff23a42d1682e284d754a022463
pear1s1ug <ca.pradom@duocuc.cl>
2025-06-22 23:26:44 -0400

    Update DatabasePerfusmartFinal.sql
    Se copió y pegó script sql correspondiente a la base de datos poblada final

--- Exp3/DatabasePerfusmartFinal.sql
+++ Exp3/DatabasePerfusmartFinal.sql
@@ -86,6 +86,84 @@ INSERT INTO `productos` VALUES (1,'La Vie Est Belle Eau de Parfum 100 ml',50000,
 UNLOCK TABLES;
 
 --
+-- Current Database: `db_cliente`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `db_cliente` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `db_cliente`;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) DEFAULT NULL,
+  `apellido` varchar(45) DEFAULT NULL,
+  `contrasenna` varchar(45) DEFAULT NULL,
+  `correo` varchar(45) DEFAULT NULL,
+  `numero` varchar(45) DEFAULT NULL,
+  `direccion` varchar(45) DEFAULT NULL,
+  `metodo_pago` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'Camila','Sánchez','C@m1l@2025','camila.sanchez@example.com','+56912345678','Av. Siempre Viva 742, Santiago','Tarjeta débito'),(2,'José','Pérez','J0s3_2025!','jose.perez@example.com','+56987654321','Calle Los Olmos 1234, Valparaíso','Tarjeta crédito'),(3,'Fernanda','Gómez','F3rn@g0m!','fernanda.gomez@example.com','+56945678912','Pasaje Las Lilas 45, Viña del Mar','Transferencia bancaria'),(4,'Ricardo','Moya','R1c4rd0!','ricardo.moya@example.com','+56933445566','Camino El Alba 3000, La Florida','Efectivo'),(5,'Isidora','Navarrete','Is1Dora@23','isidora.navarrete@example.com','+56955667788','Callejón Estrella 678, Talca','Tarjeta débito'),(6,'Matías','Castillo','M4t14s!pass','matias.castillo@example.com','+56922334455','Av. Prat 1590, Temuco','Tarjeta crédito'),(7,'Valentina','Salinas','Val3n!2025','valentina.salinas@example.com','+56966778899','Villa El Sol 203, Antofagasta','Paypal'),(8,'Sebastián','Rojas','S3bR0j@s','sebastian.rojas@example.com','+56911223344','Ruta 5 Sur Km 23, Chillán','Transferencia bancaria'),(9,'Daniela','Figueroa','Dan!F2025','daniela.figueroa@example.com','+56977889900','Pasaje Los Robles 321, Rancagua','Tarjeta débito'),(10,'Cristóbal','Muñoz','Cr1sMu2025!','cristobal.munoz@example.com','+56999887766','Calle Nueva 101, Puerto Montt','Efectivo');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Current Database: `db_logistica`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `db_logistica` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `db_logistica`;
+
+--
+-- Table structure for table `logistica`
+--
+
+DROP TABLE IF EXISTS `logistica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `logistica` (
+  `id_envio` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id_producto` int(11) DEFAULT NULL,
+  `precio` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `peso` double DEFAULT NULL,
+  `producto` varchar(255) DEFAULT NULL,
+  `fecha_envio` varchar(255) DEFAULT NULL,
+  `fecha_entrega` varchar(255) DEFAULT NULL,
+  `origen` varchar(255) DEFAULT NULL,
+  `destino` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_envio`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `logistica`
+--
+
+LOCK TABLES `logistica` WRITE;
+/*!40000 ALTER TABLE `logistica` DISABLE KEYS */;
+INSERT INTO `logistica` VALUES (1,201,45000,2,0.4,'Perfume Dior Sauvage','2025-06-01','2025-06-03','Santiago','Valparaíso'),(2,202,39000,1,0.3,'Perfume Carolina Herrera 212','2025-06-02','2025-06-04','Santiago','La Serena'),(3,203,52000,3,0.5,'Perfume Paco Rabanne Invictus','2025-06-03','2025-06-05','Valdivia','Temuco'),(4,204,61000,1,0.6,'Perfume Chanel Bleu','2025-06-04','2025-06-06','Concepción','Osorno'),(5,205,28000,4,0.2,'Perfume Adidas Dynamic Pulse','2025-06-05','2025-06-07','Santiago','Rancagua'),(6,206,85000,1,0.7,'Perfume Jean Paul Gaultier Le Male','2025-06-06','2025-06-09','Antofagasta','Copiapó'),(7,207,32000,2,0.3,'Perfume Benetton Colors Blue','2025-06-07','2025-06-10','Puerto Montt','Castro'),(8,208,18000,5,0.2,'Perfume Natura Kaiak','2025-06-08','2025-06-09','Iquique','Arica'),(9,209,70000,1,0.6,'Perfume Versace Eros','2025-06-09','2025-06-11','Santiago','Talca'),(10,210,64000,2,0.5,'Perfume Hugo Boss Bottled','2025-06-10','2025-06-13','Coquimbo','Valparaíso');
+/*!40000 ALTER TABLE `logistica` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Current Database: `db_usuario`
 --
 
@@ -131,4 +209,5 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-18 16:51:50
+-- Dump completed on 2025-06-18 19:21:34
+
