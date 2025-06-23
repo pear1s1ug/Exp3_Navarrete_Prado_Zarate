427a39df2dfbd8d4cf8d8ffbf439f25230fea1b3
pear1s1ug <ca.pradom@duocuc.cl>
2025-06-23 00:19:22 -0400

    gonzalo-navarrete sube su trabajo desde su branch

--- Exp2/PerfusmartSpringbootCrud/springboot_crud/src/main/java/com/carla/springboot/crud/springboot_crud/controllers/VentaController.java
+++ Exp2/PerfusmartSpringbootCrud/springboot_crud/src/main/java/com/carla/springboot/crud/springboot_crud/controllers/VentaController.java
@@ -1,5 +1,67 @@
 package com.carla.springboot.crud.springboot_crud.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.carla.springboot.crud.springboot_crud.entities.Venta;
+import com.carla.springboot.crud.springboot_crud.services.VentaService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/ventas")
 public class VentaController {
-    
+    @Autowired
+    private VentaService service;
+
+    @GetMapping
+    public List<Venta> List(){
+        return service.findByAll();
+    }
+    @GetMapping("/{idVenta}")
+    public ResponseEntity<?> verDetalle(@PathVariable Long idVenta){
+        Optional <Venta> ventaOptional=service.findById(idVenta);
+        if (ventaOptional.isPresent()){
+            return ResponseEntity.ok(ventaOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+
+    }
+    @PostMapping
+    public ResponseEntity<Venta> crear (@RequestBody Venta unVenta){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(unVenta));
+    }
+    @PutMapping("/{idVenta}")
+    public ResponseEntity<?> modificar(@PathVariable Long idVenta, @RequestBody Venta unVenta){
+        Optional<Venta> ventaOptional=service.findById(idVenta);
+        if (ventaOptional.isPresent()){
+            Venta ventaexistente=ventaOptional.get();
+            ventaexistente.setProducto(unVenta.getProducto());
+            ventaexistente.setPrecio(unVenta.getPrecio());
+            ventaexistente.setRut(unVenta.getRut());
+            Venta ventamodificado= service.save(ventaexistente);
+            return ResponseEntity.ok(ventamodificado);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @DeleteMapping("/{idVenta}")
+    public ResponseEntity<?> eliminar(@PathVariable Long idVenta){
+        Venta unVenta=new Venta();
+        unVenta.setidVenta(idVenta);
+        Optional<Venta> ventaOptional=service.delete(unVenta);
+        if(ventaOptional.isPresent()){
+            return ResponseEntity.ok(ventaOptional.orElseThrow());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
--- Exp2/PerfusmartSpringbootCrud/springboot_crud/src/main/java/com/carla/springboot/crud/springboot_crud/entities/Venta.java
+++ Exp2/PerfusmartSpringbootCrud/springboot_crud/src/main/java/com/carla/springboot/crud/springboot_crud/entities/Venta.java
@@ -1,5 +1,84 @@
 package com.carla.springboot.crud.springboot_crud.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="ventas")
 public class Venta {
    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_venta")
+    private Long idVenta;
+    
+    private String producto;
+    private int precio;
+    private String rut;
+
+
+
+    public Venta() {
+    }
+
+
+
+    public Venta(Long idVenta, String producto, int precio, String rut) {
+        this.idVenta = idVenta;
+        this.producto = producto;
+        this.precio = precio;
+        this.rut = rut;
+    }
+
+
+
+    public Long getidVenta() {
+        return idVenta;
+    }
+
+
+
+    public void setidVenta(Long idVenta) {
+        this.idVenta = idVenta;
+    }
+
+
+
+    public String getProducto() {
+        return producto;
+    }
+
+
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+
+
+    public int getPrecio() {
+        return precio;
+    }
+
+
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+
+
+    public String getRut() {
+        return rut;
+    }
+
+
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
 }
