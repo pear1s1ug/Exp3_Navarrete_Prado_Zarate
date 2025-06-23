09e9d4876a892023f693b0e671bf9004c39f86e9
pear1s1ug <ca.pradom@duocuc.cl>
2025-06-23 00:06:01 -0400

    carla-prado añade su trabajo en exp2 desde su branch

--- Exp2/PerfusmartSpringbootCrud/springboot_crud/src/main/java/com/carla/springboot/crud/springboot_crud/entities/Producto.java
+++ Exp2/PerfusmartSpringbootCrud/springboot_crud/src/main/java/com/carla/springboot/crud/springboot_crud/entities/Producto.java
@@ -1,5 +1,94 @@
 package com.carla.springboot.crud.springboot_crud.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="productos")
+
 public class Producto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre; 
+    private int precio; 
+    private String marca;
+    private String tipo;
+    private int cantidad; 
+
+
+
+    //constructor vacio
+    public Producto() {
+    }
+
+    //constructor lleno
+
+    public Producto(Long id, String nombre, int precio, String marca, String tipo, int cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.marca = marca;
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCantidad(){
+        return cantidad;
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+
+    
+
+    
+    //customer
+    
+    
 }
--- Exp2/PerfusmartSpringbootCrud/springboot_crud/src/main/java/com/carla/springboot/crud/springboot_crud/repository/ProductoRepository.java
+++ Exp2/PerfusmartSpringbootCrud/springboot_crud/src/main/java/com/carla/springboot/crud/springboot_crud/repository/ProductoRepository.java
@@ -1,5 +1,9 @@
 package com.carla.springboot.crud.springboot_crud.repository;
 
-public interface ProductoRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.carla.springboot.crud.springboot_crud.entities.Producto;
+
+public interface ProductoRepository extends CrudRepository<Producto, Long> {
 
 }
